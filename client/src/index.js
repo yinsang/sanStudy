670e64a2a149b77872addf38fbd6df5b1d502f42
魏银鹏 <weiyinpeng@meituan.com>
2018-05-05 02:34:06 +0800

    完善信息1-1 ， 1-2

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import "./index.css";
 import san from "san";
 import {router} from 'san-router'
+import item1_1 from './pages/item1_1';
+import item1_2 from './pages/item1_2';
 import item2_1 from './pages/item2_1';
+router.add({rule: '/item1_1', Component: item1_1, target: '#app'});
+router.add({rule: '/item1_2', Component: item1_2, target: '#app'});
 router.add({rule: '/item2_1', Component: item2_1, target: '#app'});
 router.start();
