f4b81c49b923f25b2c7f8c8ffb2f2351a3f30114
魏银鹏 <weiyinpeng@meituan.com>
2018-05-05 23:49:07 +0800

    2-2

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import "./index.css";
-import san from "san";
 import {router} from 'san-router'
-import item1_1 from './pages/item1_1';
-import item1_2 from './pages/item1_2';
-import item2_1 from './pages/item2_1';
-router.add({rule: '/item1_1', Component: item1_1, target: '#app'});
-router.add({rule: '/item1_2', Component: item1_2, target: '#app'});
-router.add({rule: '/item2_1', Component: item2_1, target: '#app'});
+import pages from './pages'
+import main from './pages/main'
+for (let key in pages) {
+  router.add({rule: `/${key}`, Component: pages[key], target: '#app'});
+}
 router.start();
+const Main = new main();
+Main.attach(document.body.querySelector("#nav"));
