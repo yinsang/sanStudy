828650ec6c9e21744834d20466c02234eaf1a44b
魏银鹏 <weiyinpeng@meituan.com>
2018-05-07 00:10:20 +0800

    item 2-3

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,7 +3,8 @@ import item1_1 from './item1_1';
 import item1_2 from './item1_2';
 import item2_1 from './item2_1';
 import item2_2 from './item2_2';
+import item2_3 from './item2_3';
 // import main from './main';
 export default {
-  item1_1, item1_2, item2_1, item2_2
+  item1_1, item1_2, item2_1, item2_2, item2_3
 }
